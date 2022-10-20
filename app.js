078f0c0511bc8f24380b526a7865cff991896d40
GiuBi2 <giulia.santos@estudante.ifms.edu.br>
2022-10-20 13:13:12 -0300

    aplicação \meunome criada

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+  console.log("Just got a request!")
+  res.send('Meu nome é Giulia Bianca Silveira dos Santos')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
